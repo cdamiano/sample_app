5eabb117cee6214ad773df2675096e524e90b288
Con Damianopoulos <con.damianopoulos@rea-group.com>
2014-09-29 15:47:39 +1000

    Finish user edit, update, index and destroy actions

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -48,9 +48,13 @@ end
 
 FactoryGirl.define do
   factory :user do
-    name "John Doe"
-    email  "john.doe@example.com"
+    sequence(:name)  { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com"}
     password "password"
     password_confirmation "password"
+
+    factory :admin do
+      admin true
+    end
   end
 end
