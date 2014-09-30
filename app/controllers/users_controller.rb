dda21120fcacca8986471f3878c351714a3373b1
Con Damianopoulos <con.damianopoulos@rea-group.com>
2014-09-30 12:16:18 +1000

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,6 +27,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def create
@@ -46,17 +47,14 @@ class UsersController < ApplicationController
     redirect_to users_url
   end
 
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
+
   private
 
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
-    end
-
-    def signed_in_user
-      unless signed_in?
-        store_location
-        redirect_to signin_url, notice: "Please sign in."
-      end
     end
 
     def correct_user
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -57,4 +57,10 @@ FactoryGirl.define do
       admin true
     end
   end
+
+  factory :micropost do
+    content "Lorem ipsum"
+    user
+  end
+
 end
